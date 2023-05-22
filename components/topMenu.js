5524d694827aba37c0c72811b23fa758c60fa131
Seowoo <seosydneyweb@gmail.com>
2023-05-22 16:45:57 +1000

    fix top menu

--- components/topMenu.js
+++ components/topMenu.js
@@ -65,7 +65,7 @@ export default function TopMenu () {
                                     <Link href="/blog">Blog</Link>
                                 </li>
                                 <li>
-                                    <Link href="/project">Project</Link>
+                                    <Link href="/portfolio">Portfolio</Link>
                                 </li>
                                 <li>
                                     <Link href="/contact">Contact</Link>
